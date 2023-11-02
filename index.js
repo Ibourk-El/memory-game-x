6cdd5faa6a610d8e0920e0a0430cbe1b52e3d013
izillid <tagmatibourk8@gmail.com>
2023-11-02 17:00:01 +0100

    /

--- index.js
+++ index.js
@@ -6,32 +6,46 @@ let countClicks = 0;
 let imgData = null;
 // let s = 0;
 // to zoom element
-img.forEach((e, i) => {
-  e.addEventListener("click", (e) => {
-    e.target.classList.toggle("active");
-    if (countClicks % 2 === 0 && countClicks < 3) {
-      e.target.classList.toggle("open");
-      imgData = e.target;
-      countClicks++;
-    } else {
-      console.log(imgData, e.target.children[0].classList[1]);
-      if (
-        imgData.children[0].classList[1] !== e.target.children[0].classList[1]
-      ) {
-        e.target.classList.toggle("open");
-        imgData.classList.toggle("open");
+img.forEach((e) => {
+  e.addEventListener( "click", ( e ) =>{
+    e.target.classList.toggle( "active" );
+    if (e.target.classList.contains( "img" ))
+    {
+      
+      if ( countClicks < 3 )
+      {
+        if ( !e.target.classList.contains( "open" ) )
+        {
+          e.target.classList.toggle( "open" );
+        }
+        if ( countClicks < 2  )
+        {
+          imgData = e.target;
+        }
+        countClicks++;
+      } else
+      {
+        // console.log(imgData,imgData.children[0].classList[1], e.target.children[0].classList[1]);
+        if (
+          imgData.children[ 0 ].classList[ 1 ] !== e.target.children[ 0 ].classList[ 1 ]
+        )
+        {
+          e.target.classList.toggle( "open" );
+          imgData.classList.toggle( "open" );
+        }
+        countClicks = 0;
+        imgData = null;
       }
-      countClicks = 0;
-      imgData = null;
     }
-  });
+  } );
 });
 // to zoom out element if use click in icon
-icons.forEach((e) => {
-  e.addEventListener("click", (e) => {
-    e.target.parentElement.classList.toggle("active");
-  });
-});
+// icons.forEach((e) => {
+//   e.addEventListener("click", (e) => {
+//     e.target.parentElement.classList.toggle( "active" );
+//     countClicks++
+//   });
+// });
 
 window.addEventListener("keydown", (e) => {
   if (e.key === "ArrowRight") {
@@ -40,7 +54,6 @@ window.addEventListener("keydown", (e) => {
   if (e.key === "ArrowLeft") {
     deg -= 90;
   }
-  build.style.transform = `rotateX(-5deg) rotateY(${deg}deg)`;
+  build.style.transform = `rotateX(-5deg) rotateY(${ deg }deg)`;
 });
 
-for (let i = 0; i < img.length; i++) {}
