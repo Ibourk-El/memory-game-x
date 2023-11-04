ba7ad12d317581b631a94118eb9c628fb3e604d2
izillid <78323417+izillid@users.noreply.github.com>
2023-11-05 00:29:16 +0100

    _

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
 let img = document.querySelectorAll(".img");
 let build = document.querySelector(".builder");
-let icons = document.querySelectorAll( "i" );
-let strBtn = document.getElementById( "start-btn" )
-let box=document.querySelectorAll( ".container" )
+let icons = document.querySelectorAll("i");
+let strBtn = document.getElementById("start-btn");
+let box = document.querySelectorAll(".container");
 let deg = -5;
 let countClicks = 0;
 let imgData = [];
 
-
-
-
 img.forEach((el) => {
   el.addEventListener("click", (e) => {
-    e.target.classList.toggle( "active" );
-    // if user click on the img 
-    if ( !e.target.classList.contains( "img" ) )
-    {
-      e.target.parentElement.classList.toggle( "active" )
+    e.target.classList.toggle("active");
+    // if user click on the img
+    if (!e.target.classList.contains("img")) {
+      e.target.parentElement.classList.toggle("active");
     }
     // normal click
-    if ( e.target.classList.contains( "active" ) )
-    {
-      if ( imgData.length<2 )
-      {
-        e.target.classList.toggle( "open" );
-        imgData.push( e.target );
-      }
-      else
-      {
+    if (e.target.classList.contains("active")) {
+      if (imgData.length < 2) {
+        e.target.classList.toggle("open");
+        imgData.push(e.target);
+      } else {
         // to check if windows has the same value
-        if(imgData[ 0 ].children[0].classList[1]!==imgData[ 1 ].children[0].classList[1])
-        {imgData[ 0 ].classList.toggle( "open" );
-          imgData[ 1 ].classList.toggle( "open" );
+        if (
+          imgData[0].children[0].classList[1] !==
+          imgData[1].children[0].classList[1]
+        ) {
+          imgData[0].classList.toggle("open");
+          imgData[1].classList.toggle("open");
+        } else {
+          showIcons(
+            imgData[0].children[0].className,
+            imgData[1].children[0].className
+          );
         }
         // reset datas
         imgData = [];
-        e.target.classList.toggle( "open" );
-        imgData = [ e.target ];
+        e.target.classList.toggle("open");
+        imgData = [e.target];
       }
     }
-    
   });
 });
 window.addEventListener("keydown", (e) => {
@@ -50,7 +49,7 @@ window.addEventListener("keydown", (e) => {
     deg -= 90;
   }
   build.style.transform = `translate(-50%)  rotateX(-5deg) rotateY(${deg}deg)`;
-} );
+});
 
 // function clickMousePsition (x,y)
 // {
@@ -63,28 +62,28 @@ window.addEventListener("keydown", (e) => {
 //         document.body.style.zoom="100%"
 //       }, 1000);
 // }
-strBtn.addEventListener( "click", () =>
-{
-  let num = parseInt( document.getElementById( "input-num" ).value )
-  let playPage = document.querySelector( ".play-page" )
-  if ( num !== 0 )
-  {
-    for ( let j = 0; j < box.length; j++ )
-    {
-      for ( let i = 0; i < 9; i++ )
-      {
-        if ( i < num )
-        {
-          box[ j ].children[ j ].width = `${ Math.floor( 100 / num ) - 5 }%`
-          box[ j ].children[ j ].height = `${ Math.floor( 100 / num ) - 5 }%`
-        }
-        else
-        {
-          box[ j ].children[ i ].style.display = "none";
-        }
+strBtn.addEventListener("click", () => {
+  let num = parseInt(document.getElementById("input-num").value);
+  let playPage = document.querySelector(".play-page");
+  if (num !== 0) {
+    for (let j = 0; j < box.length; j++) {
+      for (let i = num; i < 9; i++) {
+        box[j].children[i].style.display = "none";
       }
-      
     }
-    playPage.style.display = "none"
+    playPage.style.display = "none";
   }
-} )
+});
+
+async function showIcons(icon1, icon2) {
+  document.querySelector(".show-img-box").style.display = "flex";
+  let showIcon = document.querySelectorAll(".show-img i");
+  let tr = setInterval(() => {
+    showIcon[0].className = icon1;
+    showIcon[1].className = icon2;
+  }, 0);
+  setTimeout(() => {
+    clearInterval(tr);
+    document.querySelector(".show-img-box").style.display = "none";
+  }, 2000);
+}
